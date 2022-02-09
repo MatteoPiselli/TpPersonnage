1a71f70a12aae1302cb2c509fd69dbeca3305ecc
MatteoPiselli <98105082+MatteoPiselli@users.noreply.github.com>
2022-02-09 23:20:03 +0100

    Create Personnage.java

---
+++ Personnage.java
@@ -0,0 +1,41 @@
+package Personnage;
+
+public class Personnage {
+	// les attributs
+	private String nom;
+	private int vie;
+
+	// le constructeur
+	public Personnage(String name, int life) {
+		this.nom = name;
+		this.vie = life;
+	}
+
+	// accesseur
+	public String getNom() {
+		return nom;
+	}
+
+	public int getVie() {
+		return vie;
+	}
+
+	public void setVie(int vie) {
+		this.vie=vie;
+	}
+
+	public boolean degat(int nb){
+		setVie(this.vie-nb);
+		if(getVie()>0)
+			return true;
+		return false;
+	}
+
+	public int attack(){
+		return vie;
+	}
+	public int attack(int pt){
+		return pt;
+	}
+}
+
