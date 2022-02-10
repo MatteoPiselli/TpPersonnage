9402abf6063530922ff1e06a49b4c65d784cd787
MatteoPiselli <98105082+MatteoPiselli@users.noreply.github.com>
2022-02-10 09:37:14 +0100

    Update jeuPersonnage.java

--- jeuPersonnage.java
+++ jeuPersonnage.java
@@ -6,28 +6,30 @@ import Personnage.*;
             Personnage UnPilier = new Personnage ("Tanjiro", 200);
             Personnage UneLuneSuperieur = new Personnage ("Muzan", 500);
 
-            System.out.println("Le pourfendeur " + UnPilier.getNom() + " (" + UnPilier.getVie() + " HP), est à la recherche d'un démon se trouvant sur le Mont Fuji et qu'il doit décapiter. Tel est sa nouvelle mission");
+            System.out.println("\nLe pourfendeur " + UnPilier.getNom() + " (" + UnPilier.getVie() + " HP), est à la recherche d'un démon se trouvant sur le Mont Fuji et qu'il doit décapiter. Tel est sa nouvelle mission");
 
-            System.out.println("La nuit tombée, il tombe nez à nez avec le démon");
+            System.out.println("\nLa nuit tombée, il tombe nez à nez avec le démon");
 
-            System.out.println("Le démon est : " + UneLuneSuperieur.getNom() + " (" + UneLuneSuperieur.getVie() + " HP)");
+            System.out.println("\nLe démon est : " + UneLuneSuperieur.getNom() + " (" + UneLuneSuperieur.getVie() + " HP)");
 
-            System.out.println(UnPilier.getNom() + " attaque " + UneLuneSuperieur.getNom() + " avec un sabre (dégâts " +  UnPilier.attack(100) + ") " + UneLuneSuperieur.getNom() + " en vie ? " + UneLuneSuperieur.degat(UnPilier.attack(100)) + " (" + UneLuneSuperieur.getVie() + " HP)");
+            System.out.println("\n" + UnPilier.getNom() + " attaque " + UneLuneSuperieur.getNom() + " avec un sabre (dégâts " +  UnPilier.attack(100) + ") " + UneLuneSuperieur.getNom() + " en vie ? " + UneLuneSuperieur.degat(UnPilier.attack(100)) + " (" + UneLuneSuperieur.getVie() + " HP)");
 
-            System.out.println(UneLuneSuperieur.getNom() + " ricane");
+            System.out.println("\n" + UneLuneSuperieur.getNom() + " ricane");
 
-            System.out.println(UneLuneSuperieur.getNom() + " riposte en se ruant sur " + UnPilier.getNom() + " et enlève (dégâts " + UneLuneSuperieur.attack(150) + ") " + UnPilier.getNom() + " en vie ? " + UnPilier.degat(UneLuneSuperieur.attack(150)) + " (" + UnPilier.getVie() + "HP)");
+            System.out.println("\n" + UneLuneSuperieur.getNom() + " riposte en se ruant sur " + UnPilier.getNom() + " et enlève (dégâts " + UneLuneSuperieur.attack(150) + ") " + UnPilier.getNom() + " en vie ? " + UnPilier.degat(UneLuneSuperieur.attack(150)) + " (" + UnPilier.getVie() + "HP)");
 
-            System.out.println(UnPilier.getNom() + " mal en point donne tout ce qu'il a dans sa technique la plus puissante afin de pouvoir décapiter le démon, 'Hinokami Kagura' sur " + UneLuneSuperieur.getNom() + " (dégâts " + UnPilier.attack(400) + ") " + UneLuneSuperieur.getNom() + " en vie ? " + UneLuneSuperieur.degat(UnPilier.attack(400)) + " (" + UneLuneSuperieur.getVie() + " HP)");
+            System.out.println("\n" + UnPilier.getNom() + " mal en point donne tout ce qu'il a dans sa technique la plus puissante afin de pouvoir décapiter le démon, 'Hinokami Kagura' sur " + UneLuneSuperieur.getNom() + " (dégâts " + UnPilier.attack(400) + ") " + UneLuneSuperieur.getNom() + " en vie ? " + UneLuneSuperieur.degat(UnPilier.attack(400)) + " (" + UneLuneSuperieur.getVie() + " HP)");
 
             if(UnPilier.getVie()>0)
-                System.out.println(UnPilier.getNom() + " -> aaaa..aaaa.mission...réussie");
+                System.out.println("\n" + UnPilier.getNom() + " -> aaaa..aaaa.mission...réussie");
             else
-                System.out.println(UnPilier.getNom() + " -> arrgh!!");
+                System.out.println("\n" + UnPilier.getNom() + " -> arrgh!!");
 
             if(UneLuneSuperieur.getVie()>0)
-                System.out.println(UneLuneSuperieur.getNom() + " -> je suis immortel après tout, hahahah");
+                System.out.println("\n" + UneLuneSuperieur.getNom() + " -> je suis immortel après tout, hahahah");
             else
-                System.out.println(UneLuneSuperieur.getNom() + " -> impo..ssi..ble..arrgh!!");
+                System.out.println("\n" + UneLuneSuperieur.getNom() + " -> impo..ssi..ble..arrgh!!");
+            
+            System.out.println("\n");
         }
     }
